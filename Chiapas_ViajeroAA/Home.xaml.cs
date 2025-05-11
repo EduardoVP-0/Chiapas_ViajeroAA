54041fd8d7c6aaa51a5a8ae4f869e1225e826965
TheMiranda02 <jose.guzman26@unach.mx>
2025-05-11 02:06:45 -0600

    VERSION5

--- Chiapas_ViajeroAA/Home.xaml.cs
+++ Chiapas_ViajeroAA/Home.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Pagina_Principal
 {
@@ -19,9 +12,57 @@ namespace Pagina_Principal
     /// </summary>
     public partial class Home : Window
     {
+        private List<string> imagenes = new List<string>
+        {
+            "Banner1.png",
+            "Banner2.png"
+        };
+
+        private int indice = 0;
+        private DispatcherTimer temporizador;
+
         public Home()
         {
             InitializeComponent();
+            IniciarCarrusel();
+        }
+
+        private void IniciarCarrusel()
+        {
+            temporizador = new DispatcherTimer();
+            temporizador.Interval = TimeSpan.FromSeconds(5);
+            temporizador.Tick += CambiarImagen;
+            temporizador.Start();
+
+            CambiarImagen(null, null); // Mostrar la primera imagen de inmediato
+        }
+
+        private void CambiarImagen(object sender, EventArgs e)
+        {
+            try
+            {
+                string ruta = imagenes[indice];
+                CarruselImage.Source = new BitmapImage(new Uri(ruta, UriKind.Relative));
+
+                // Aplicar la animación de opacidad (fade)
+                Storyboard fadeStoryboard = (Storyboard)FindResource("FadeStoryboard");
+                CarruselImage.BeginStoryboard(fadeStoryboard);
+
+                // Siguiente índice
+                indice = (indice + 1) % imagenes.Count;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar la imagen del carrusel: " + ex.Message);
+            }
+        }
+
+        private void BtnSalir(object sender, RoutedEventArgs e)
+        {
+            MainWindow VentanaSalir = new MainWindow();
+            VentanaSalir.Show();
+
+            this.Close();
         }
     }
 }
