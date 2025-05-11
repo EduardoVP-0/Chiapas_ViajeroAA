a2cd46b97327c3d2ebaf05777e1afe2afcb75f38
EduardoVP-0 <eduardo.velazquez71@unach.mx>
2025-05-11 02:08:42 -0600

    VERSION 6

---
+++ Chiapas.ViajeroAA.Conexion/RepositorioOperadora.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Chiapas.ViajeroAA.Conexion;
+
+namespace Chiapas.ViajeroAA.Conexion
+{
+    public class RepositorioOperadora
+    {
+        private Conexion _conexion;
+        public RepositorioOperadora()
+        {
+            _conexion = new Conexion();
+        }
+
+        public List<VistaOperadora> ObtenerOperadoras()
+        {
+            var lista = new List<VistaOperadora>();
+
+            using (var conn = _conexion.ObtenerConexion())
+            {
+                string query = "SELECT nombre_operadora, sitio_web, direccion, representante, email FROM operadora";
+
+                using (var cmd = new MySqlCommand(query, conn))
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        lista.Add(new VistaOperadora
+                        {
+                            NombreOperadora = reader.GetString("nombre_operadora"),
+                            SitioWeb = reader.GetString("sitio_web"),
+                            Direccion = reader.GetString("direccion"),
+                            Representante = reader.GetString("representante"),
+                            Email = reader.GetString("email")
+                        });
+                    }
+                }
+            }
+
+            return lista;
+        }
+    }
+}
---
+++ Chiapas.ViajeroAA.Conexion/VistaOperadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chiapas.ViajeroAA.Conexion
+{
+    public class VistaOperadora
+    {
+        public string NombreOperadora { get; set; }
+        public string SitioWeb { get; set; }
+        public string Direccion { get; set; }
+        public string Representante { get; set; }
+        public string Email { get; set; }
+
+    }
+}
